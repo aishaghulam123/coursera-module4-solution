342167e91909bde963d9b93b1a24f6d4e8fb60a9
Aisha muhammad ghulam <112826366+aishaghulam123@users.noreply.github.com>
2024-02-18 21:20:12 +0500

    Add files via upload

---
+++ coursera module4/script.js
@@ -0,0 +1,11 @@
+var names=["Yaakov", "John", "Joly", "jason", "Angel","Rose ","Jess", "Frank","jake", "Harry", "paula", "Laura", "jim"];
+
+
+for (var i =0; i < names.length; i++) {
+	if(names[i].charAt(0)=='J'|| names[i].charAt(0)=='j'){
+	    console.log("Good Bye "+ names[i])
+	}
+   else{
+   	   console.log("Hello "+ names[i])
+   }
+}
